a225bdab797f058f3b63ae9105a2184670d5dcae
Charith Lakshan <it23841086@my.sliit.lk>
2025-08-23 16:57:42 +0530

    Create RegistrationScreen

--- app/src/main/java/com/example/ceylonbotanica/MainActivity.kt
+++ app/src/main/java/com/example/ceylonbotanica/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             delay(3000)
-            startActivity(Intent(this@MainActivity, LoginScreen::class.java))
+            startActivity(Intent(this@MainActivity, RegistrationScreen::class.java))
             finish()
         }
     }
