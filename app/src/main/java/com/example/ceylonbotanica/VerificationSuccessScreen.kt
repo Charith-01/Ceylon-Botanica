6860cda8801ce54a34d23ec5bfbd0b54e864f24e
Charith Lakshan <it23841086@my.sliit.lk>
2025-08-26 09:14:39 +0530

    Make page navigate

--- app/src/main/java/com/example/ceylonbotanica/VerificationSuccessScreen.kt
+++ app/src/main/java/com/example/ceylonbotanica/VerificationSuccessScreen.kt
@@ -1,6 +1,8 @@
 package com.example.ceylonbotanica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class VerificationSuccessScreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btnCntinue).setOnClickListener {
+            val intent = Intent(this, HomeScreen::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
     }
 }
